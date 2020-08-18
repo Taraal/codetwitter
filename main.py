a00df7c9e0813e1c9cdc2935c123ca3e504831da
Sylouan Corfa <sylouan.corfa@lengow.com>
2020-08-18 16:09:55 +0200

    Got full tweet text and photo urls

--- main.py
+++ main.py
@@ -21,10 +21,20 @@ access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
 auth = OAuth1(oauth_consumer_key, api_secret, oauth_token, access_token_secret)
 
+payload = {"tweet_mode": "extended"}
+response = requests.get(url, auth=auth, params=payload)
 
-response = requests.get(url, auth=auth)
+#with open("test.txt", "w") as outfile:
+#    json.dump(json.loads(response.text), outfile)
 
-with open("test.txt", "w") as outfile:
-    json.dump(json.loads(response.text), outfile)
-
-print("\u00c9")
+for tweet in json.loads(response.text):
+    print("User : " + tweet["user"]["name"])
+    print("Name : " + tweet["user"]["screen_name"])
+    print("Text : " + tweet["full_text"])
+    print("Likes : " + str(tweet["favorite_count"]))
+    print("Retweets : " + str(tweet["retweet_count"]))
+    medias = tweet['entities'].get("media")
+    if medias:
+        for index, medium in enumerate(medias):
+            print("Photo " + str(index) + " : " + medium['media_url'])
+    print("-------------------------------------------")
